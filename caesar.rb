6d9c741c1e961d0aa5e789cc17b1401abbc0e7d0
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-10 08:35:15 -0700

    caesar

--- caesar.rb
+++ caesar.rb
@@ -1,13 +1,27 @@
+require "pry"
 class Caesar
+  attr_reader :rotator
+
   CIPHER_UPPERCASE = ("A".."Z").to_a
   CIPHER_LOWERCASE = ("a".."z").to_a
   def initialize(rotator, message)
     @rotator = rotator
     @message = message
+    @encrypted_message = ""
   end
 
   def encrypt
-    
+    @message.each do |word|
+      word.chars.each do |char|
+        @encrypted_message += rotate_letter(char)
+      end
+    end
+    binding.pry
+  end
+
+  def rotate_letter(letter)
+    new_index = CIPHER_LOWERCASE.index(letter.downcase) + rotator.to_i
+    CIPHER_LOWERCASE[new_index]
   end
 
 
